98f417cbb88af607a342ff86bbbac788c270151a
Luiz <luizlggustavo@hotmail.com>
2020-06-02 20:44:51 -0300

    initial auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MercadoBitcoinModule } from './modules/mercadobitcoin/mercadobitcoin.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersService } from './modules/users/users.service';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    
-    MercadoBitcoinModule
+    MercadoBitcoinModule,
+    AuthModule,
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+  ],
+  providers: [
+    AppService,
+    UsersService,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { jwtConstants } from './constants';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  exports: [
+    AuthService,
+  ],
+})
+export class AuthModule { }
